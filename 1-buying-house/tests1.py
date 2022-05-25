2601a61eefd4a0189dc42e4b3953da382d2c2cb7
Vladimir Strukov <deepcopy.job@gmail.com>
2022-05-25 17:31:06 +0300

    Change comments

--- 1-buying-house/tests1.py
+++ 1-buying-house/tests1.py
@@ -8,37 +8,42 @@ def main():
     villa = House(500, 500000)
     residence = House(2500, 2500000)
 
-    # Создание маленького домика.
-    bungalow = SmallHouse(50000)
-
-    # Создание человека.
-    vladimir = Human('Vladimir', 22)
-
-    # Тестирование.
+    # 1. Вызов справочного метода default_info() для класса Human.
     print('\n--- default_info ---')
     Human.display_default_info()
 
+    # 2. Создание объекта класса Human.
+    vladimir = Human('Vladimir', 22)
+
+    # 3. Вывод справочной информации о созданном объекте (метод info()).
     print('\n--- info ---')
     vladimir.display_info()
+
+    # 4. Создание объекта класса SmallHouse.
+    bungalow = SmallHouse(50000)
 
     print(f'\n--- earn_money ---')
-    vladimir.earn_money(50000.0)
+    vladimir.earn_money(20000.0)
 
-    print(f'\n--- buy_house ---')
-    vladimir.buy_house(bungalow, 5)
+    # 5. Попытка купить дом, получение предупреждения.
+    # print(f'\n--- buy_house ---')
+    # vladimir.buy_house(bungalow, 5)
 
     print('\n--- info ---')
     vladimir.display_info()
 
+    # 6. Зарабатываем денег.
     print(f'\n--- earn_money ---')
-    vladimir.earn_money(150000.0)
+    vladimir.earn_money(250000.0)
 
     print('\n--- info ---')
     vladimir.display_info()
 
+    # 7. Снова пробуем купить дом.
     print(f'\n--- buy_house ---')
     vladimir.buy_house(cottage, 10)
 
+    # 8. Проверяем состояние объекта vladimir класса Human.
     print('\n--- info ---')
     vladimir.display_info()
 
